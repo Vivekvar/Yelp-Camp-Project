b2b958f1d533ada73c15e0849ee658081e71c8d9
Vivek Varshney <vivekvarshney40hd@gmail.com>
2022-01-24 23:23:11 +0530

    Added Index Route

--- app.js
+++ app.js
@@ -31,6 +31,13 @@ app.set('view engine', 'ejs');
 app.get('', (req, res) => {
     res.send('HI!');
 })
+
+// READ
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
+})
+
 app.listen(3000, () => {
     console.log("Listening on port 3000!!!");
 })

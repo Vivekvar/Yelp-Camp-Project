479edd633ad24252551f707ed3dbf087cf032e86
Vivek Varshney <vivekvarshney40hd@gmail.com>
2022-01-25 20:16:29 +0530

    Styled Show Page

--- seeds/index.js
+++ seeds/index.js
@@ -21,7 +21,7 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random = Math.floor(Math.random() * 1000);
-        const price = Math.floor(Math.random() * 20000) + 10000;
+        const price = Math.floor(Math.random() * 2000) + 1000;
         const camp = new Campground({
             location: `${cities[random].city}, ${cities[random].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,

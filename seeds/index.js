c95285dc3dfe51b86fcc555ec5eef10d90ab4af3
Vivek Varshney <vivekvarshney40hd@gmail.com>
2022-01-25 19:28:52 +0530

    Styled Index Page

--- seeds/index.js
+++ seeds/index.js
@@ -17,7 +17,6 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
-
 const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
